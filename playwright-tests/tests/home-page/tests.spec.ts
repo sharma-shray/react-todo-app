9b32c48e909a7bbc9a56df635fb8ad37af5f9854
sharma-shray <sharma.shray92@gmail.com>
2024-04-26 17:39:48 +0300

    more tests

--- playwright-tests/tests/home-page/tests.spec.ts
+++ playwright-tests/tests/home-page/tests.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { text } from 'stream/consumers';
 
 test('Home page title', async ({ page }) => {
   await page.goto('localhost:3000');
@@ -32,4 +33,36 @@ test('input field placeholder text', async ({ page }) => {
   } else {
     throw new Error('Input element with class "add-todo" not found');
   }
+});
+
+test('Buttons has the text', async ({ page }) => {
+  await page.goto('localhost:3000');
+
+   const allButtonText = await page.textContent('.filters li:nth-child(1) a');
+   const activeButtonText = await page.textContent('.filters li:nth-child(2) a');
+   const completedButtonText = await page.textContent('.filters li:nth-child(3) a');
+ 
+   expect(allButtonText).toBe('All');
+   expect(activeButtonText).toBe('Active');
+   expect(completedButtonText).toBe('Completed');
+
+});
+
+test('Adding a task increases the item count', async ({ page }) => {
+    await page.goto('localhost:3000');
+
+    const initialItemCount = await page.textContent('.pull-left');
+  
+    await page.fill('input[type="text"]', 'New Task');
+    await page.click('a.button.add');
+  
+    await page.waitForTimeout(1000);
+  
+    const updatedItemCount = await page.textContent('.pull-left');
+  
+    // Convert item count strings to numbers
+    const initialItemCountNumber = parseInt(initialItemCount!);
+    const updatedItemCountNumber = parseInt(updatedItemCount!);
+  
+    expect(updatedItemCountNumber).toBe(initialItemCountNumber + 1);
 });
